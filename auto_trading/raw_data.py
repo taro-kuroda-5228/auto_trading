ecf00d711ed35648266badecc55c4415d578e5ca
taro-kuroda-5228 <dancing205228@gmail.com>
2021-11-06 13:45:07 +0900

    fix: raw_data.py

--- auto_trading/raw_data.py
+++ auto_trading/raw_data.py
@@ -1,22 +1,26 @@
+"""RawData class is included.
+"""
+
 import datetime
 import pandas as pd
 from yahoo_finance_api2 import share
 
-"""Module to get stock price information during the wanted duration.
-
-In this class, get price datas including datetime, timestamp, open, high, low, close, and volume
-
-Args:
-    name: companies name
-    nationality: listed country
-    start_date: date wanted information
-    days: how long days information you want before start_date
-
-Typical usage example:
-raw_data = RawData(name, nationality, start_date, days).time_series_data
-"""
-
 class RawData:
+    
+    """Module to get stock price information during the wanted duration.
+
+    In this class, get price datas including datetime, timestamp, open, high, low, close, and volume
+
+    Args:
+        name: company's name
+        nationality: listed country
+        start_date: date information wanted
+        days: how long days information you want before start_date
+
+    Typical usage example:
+    raw_data = RawData(name, nationality, start_date, days).time_series_data
+    """
+    
     def __init__(self, name: str, nationality: str, start_date: str, days: int):
         self.name = name
         self.nationality = nationality
@@ -24,9 +28,11 @@ class RawData:
         self.days = days
     
     def add_suffix_tse(self)-> str:
+        
         """Function only for use in time_series_data
         If nationality is Japan, add '.T' at the end of name
         """
+        
         if self.nationality == 'JP':
             if self.name[-2:] != '.T':
                 self.name += '.T'
@@ -40,7 +46,8 @@ class RawData:
 #         return df
 
     @property
-    def time_series_data(self)-> pd.DataFrame:       
+    def time_series_data(self)-> pd.DataFrame: 
+        
     """Make timeseries table.
 
     return 
@@ -48,6 +55,7 @@ class RawData:
         index = ['datetime'], type = '%Y-%m-%d', ascending=False
 
     """
+    
         self.add_suffix_tse
          
         my_share = share.Share(self.name)
@@ -68,5 +76,6 @@ class RawData:
         end_date = pd.to_datetime(self.start_date)-datetime.timedelta(days=self.days-1)
         end_date = end_date.strftime('%Y-%m-%d')
         df = df.loc[self.start_date:end_date,:]
+        
         return df
     
